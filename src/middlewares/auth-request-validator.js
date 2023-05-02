bccffe9ae699ccb86f8050d135eac3bbcf82722f
pankaj <pankajsemwalpankaj123@gmail.com>
2023-05-02 17:34:05 +0530

    Added request validate middleware

---
+++ src/middlewares/auth-request-validator.js
@@ -0,0 +1,17 @@
+const validateUserAuth= (req,resp,next)=>{
+    if(!req.body.email || !req.body.password)
+    {
+        return resp.status(400).json({
+            success:false,
+            data:{},
+            message:'something went wrong',
+            err: 'email or password missing '
+        })
+    }
+}
+
+module.exports={
+
+    validateUserAuth
+    
+}
